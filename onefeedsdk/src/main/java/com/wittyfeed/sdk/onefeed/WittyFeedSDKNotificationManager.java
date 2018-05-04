3685d5a88d3b757834d7ce32b12bc96b7a96462a
aishwarydhare <aishwary.dhare@gmail.com>
2018-05-04 19:13:48 +0530

    minor fix for crash on notification received

--- onefeedsdk/src/main/java/com/wittyfeed/sdk/onefeed/WittyFeedSDKNotificationManager.java
+++ onefeedsdk/src/main/java/com/wittyfeed/sdk/onefeed/WittyFeedSDKNotificationManager.java
@@ -7,6 +7,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -15,6 +16,7 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.preference.PreferenceManager;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -317,8 +319,13 @@ public class WittyFeedSDKNotificationManager {
         this.homeScreenIntent = homeScreenIntent;
         WittyFeedSDKSingleton.getInstance().homeActivityIntent = homeScreenIntent;
         String classPassedInIntent = homeScreenIntent.getComponent().getClassName();
-        WittyFeedSDKSingleton.getInstance().editor_sharedPref.putString("homeScreenIntentClassName", classPassedInIntent).commit();
-
+        SharedPreferences.Editor prefsEditor;
+        if(WittyFeedSDKSingleton.getInstance().editor_sharedPref == null){
+            prefsEditor = PreferenceManager.getDefaultSharedPreferences(application).edit();
+        } else {
+            prefsEditor = WittyFeedSDKSingleton.getInstance().editor_sharedPref;
+        }
+        prefsEditor.putString("homeScreenIntentClassName", classPassedInIntent).apply();
     }
 
 
